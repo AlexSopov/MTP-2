134331b38a36f59eb5303ad938bf8a0041f27f10
Anthony <stupnitskiy.reserve@gmail.com>
2018-03-11 00:50:21 +0200

    Add EventHandler

--- FileSearcher/src/sample/Controller.java
+++ FileSearcher/src/sample/Controller.java
@@ -1,4 +1,42 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private CheckBox checkbox1;
+    @FXML
+    private CheckBox checkbox2;
+    @FXML
+    private CheckBox checkbox3;
+
+    @FXML
+    private ListView listview;
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    public void initialize(){
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(checkbox3.isSelected()) {
+                    button.setText(textfield.getText());
+                }
+            }
+        });
+
+    }
+
 }
